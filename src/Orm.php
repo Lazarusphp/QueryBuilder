e7dc49e4c5b79f90d3460e1304fe78ccd3451eca
martin bamber <mbamber1986@gmail.com>
2024-08-25 12:35:33 +0100

    Added orm Base file

---
+++ src/Orm.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Lazarusphp\Orm;
+
+use LazarusPhp\DatabaseManager\Database;
+use LazarusPhp\Orm\Traits\Select;
+
+class Orm extends Database
+{
+
+    // Load Trait Files;
+    // Generate the Param Values
+    public $param = [];
+    public $sql;
+    public $table;
+    private $flags = [];
+
+
+    //Use Traits
+    use Select;
+    public function __construct()
+    {
+        Parent::__construct();
+        // Instantiate a Blank $sql statement
+        $this->sql = "";
+    }
+
+
+    public function table($table,$callable=null)
+    {
+        $this->table = $table;
+        if(!is_null($callable) && is_callable($callable))
+        {
+            return $callable($this);
+        }
+        else
+        {
+            return $this;
+        }
+    }
+
+    public function bind($key, $value)
+    {
+        $this->param[$key] = $value;
+        return $this;
+    }
+
+
+
+
+    private function newFlag($name, $value = true)
+    {
+        if (!isset($this->flags[$name])) {
+            $this->flags[$name] = $value;
+        } else {
+            trigger_error("Flag $name Already Created");
+        }
+    }
+
+    private function displayFlag($name)
+    {
+        if(array_key_exists($name,$this->flags))
+        {
+            return true;
+        }
+        else
+        {
+            trigger_error("Flag Doesnt Exist");
+        }
+    }
+
+    public function generateUid($name)
+    {
+        // generate Param Name;
+        return uniqid($name . "_");
+    }
+
+    public function sqlLoader()
+    {
+        if($this->displayFlag("select"))
+        {
+            echo "Sql Loader will run Select Statement";
+        }
+        return $this;
+    }
+
+    // Get All
+    public function get()
+    {
+        $this->sql($this->sql, $this->param);
+        $this->All();
+    }
+
+    // Get 1
+    public function first()
+    {
+        $this->sql($this->sql, $this->param);
+        $this->One();
+    }
+
+    public function toJson($print = false)
+    {
+        ($print == true) ? header('Content-Type: application/json; charset=utf-8') : false;
+        return json_encode($this->param);
+    }
+}
