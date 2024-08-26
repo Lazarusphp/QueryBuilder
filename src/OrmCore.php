d34b0530af94bedf6bc6d734feca8536d69edee7
martin bamber <mbamber1986@gmail.com>
2024-08-27 00:21:56 +0100

    Added Where Support to codebase

---
+++ src/OrmCore.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace Lazarusphp\Orm;
+
+use LazarusPhp\DatabaseManager\Database;
+use LazarusPhp\Orm\Interfaces\OrmInterface;
+use LazarusPhp\Orm\Traits\Controllers\Insert;
+use LazarusPhp\Orm\Traits\Controllers\Select;
+use LazarusPhp\Orm\Traits\Controllers\Update;
+
+class OrmCore extends Database implements OrmInterface
+{
+
+    // Load Trait Files;
+    use Insert;
+    use Select;
+    use Update;
+    // Generate the Param Values
+    public $param = [];
+    public $sql;
+    public $table;
+    private $flags = [];
+    private $values;
+    public $where = [];
+
+
+    public $data = [];
+
+    public function __set($name, $value)
+    {
+        $this->param[$name] = $value;
+    }
+
+
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->param)) {
+            return $this->param[$name];
+        }
+    }
+
+    public function __isset($name)
+    {
+        return isset($this->param[$name]);
+    }
+
+    public function __unset($name)
+    {
+        $uid = uniqid("$name_");
+        unset($this->param[$uid]);
+    }
+
+
+    public function __construct()
+    {
+        Parent::__construct();
+        // Instantiate a Blank $sql statement
+        $this->sql = "";
+    }
+
+
+
+    public function validateFilters()
+    {
+        if (count($this->allowed) > 0) {
+            $allowed = array_diff($this->allowed, $this->filtered);
+            return implode(", ", $allowed);
+        } else {
+            return "*";
+        }
+        // if (count($this->allowed) > 0) {
+        //     $allowed = array_diff($this->allowed, $this->filtered);
+        //     return implode(", ", $allowed);
+        // } else {
+        //     return "*";
+        // }
+    }
+
+
+    public function table($table, $callable = null)
+    {
+        $this->table = $table;
+        if (!is_null($callable) && is_callable($callable)) {
+            return $callable($this);
+        } else {
+            return $this;
+        }
+    }
+
+    public function bind($key, $value)
+    {
+        $this->param[$key] = $value;
+        return $this;
+    }
+
+    public function delete() {}
+
+
+    public function toSql()
+    {
+        echo $this->sql;
+    }
+
+
+
+    private function newFlag($name, $value = true)
+    {
+        if (!isset($this->flags[$name])) {
+            $this->flags[$name] = $value;
+        } else {
+            trigger_error("Flag $name Already Created");
+        }
+    }
+
+    private function displayFlag($name)
+    {
+        if (array_key_exists($name, $this->flags)) {
+            return true;
+        } else {
+            return false;
+            trigger_error("Flag Doesnt Exist");
+        }
+    }
+
+    public function generateUid($name)
+    {
+        // generate Param Name;
+        return uniqid($name . "_");
+    }
+
+    public function save()
+    {
+        if ($this->displayFlag("select")) {
+            // echo "select is chosen";
+            $this->Fetchwhere();
+        } elseif ($this->displayFlag("insert")) {
+            echo "insert Chosen";
+         
+        } elseif ($this->displayFlag("update")) {
+            echo "update";
+            $this->setValues();
+            $this->Fetchwhere();
+            echo $this->sql;
+        } elseif ($this->displayFlag("delete")) {
+            echo "delete";
+        }
+
+        // Push Content
+        // echo $this->sql;
+        return $this->GenerateQuery($this->sql,$this->param);
+    }
+
+    // Start Where()
+    public function where($key,$value,$operator=null)
+    {
+        $operator = $operator ?? "=";
+        $params = uniqid("where_");
+         $condition = $key.$operator.":$params";
+          if(count($this->where))
+          {
+              $condition = " AND " . $condition;
+          }
+          $this->where[] = $condition;
+          $this->param[$params] = $value;
+          return $this;
+    }
+    public function FetchWhere()
+    {
+        $wherecond = [];
+        $wheres = $this->where;
+        if(!empty($wheres))
+        {
+        $this->sql .= " WHERE ";
+         foreach($wheres as $where)
+            {
+                $wherecond[] = $where;
+            }
+        $this->sql .= implode(" ",$wherecond);
+        }
+        else
+        {
+          
+        }
+         return $this;
+    }
+    // End WHere
+
+    // Get All
+    public function get()
+    {
+        if ($this->displayFlag("select")) {
+            $this->FetchWhere();
+            return $this->GenerateQuery($this->sql, $this->param)->fetchAll();
+        }
+    }
+
+    // Get 1
+    public function first()
+    {
+        if ($this->displayFlag("select")) {
+            $this->FetchWhere();
+            $query = $this->GenerateQuery($this->sql, $this->param);
+            return $query->fetch();
+        }
+    }
+
+    public function toJson($print = false)
+    {
+        ($print == true) ? header('Content-Type: application/json; charset=utf-8') : false;
+        return json_encode($this->param);
+    }
+}
