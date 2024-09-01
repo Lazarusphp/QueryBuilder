952566a7b84424370bfc73b8c53442b81a5f7a3a
martin bamber <mbamber1986@gmail.com>
2024-09-01 13:23:30 +0100

    Created new FindById and findOrFail method in select trait

--- src/Traits/Controllers/Select.php
+++ src/Traits/Controllers/Select.php
@@ -4,12 +4,49 @@ namespace LazarusPhp\Orm\Traits\Controllers;
 
 trait Select
 {
+    protected $args;
     public function select(...$args)
     {      
-            if(count($args) > 0)
-            (count($args) > 0) ? $args = implode(",",$args) : $args = "*";
-            $this->sql .= "SELECT $args FROM " . $this->table;
+  
+            (count($args) > 0) ? $this->args = implode(",",$args) : $this->args = "*";
+            $this->sql .= "SELECT $this->args FROM " . $this->table;
             return $this;
+    }
+
+
+    public function findById($id)
+    {
+      $query = $this->select()->where("id",$id)->save();
+      if($query->rowCount() === 1)
+      {
+        return $query->fetch();
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    /**
+     * Find or failed
+     *
+     * @param [int] $id
+     * select value based on the user id;
+     * return true or false; does not pass any values
+     * cal
+     * @return void
+     */
+    public function findOrFail(int $id):bool
+    {
+          $query = $this->select()->where("id",$id)->save();
+          if($query->rowCount() === 1)
+          {
+                return true;
+          } 
+          else
+          {
+            return false;
+          }
     }
 
     public function as($alias)
@@ -17,4 +54,6 @@ trait Select
         $this->sql .= " AS $alias ";
         return $this;
     }
+
+
 }
