bb34fb0c3c6df6aa55052f7498bc3a6539e2befd
martin bamber <mbamber1986@gmail.com>
2024-09-04 00:22:26 +0100

    Added Joins to Conditional Traits

---
+++ src/Traits/Conditions/Joins.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace LazarusPhp\Orm\Traits\Conditions;
+
+trait Joins
+{
+    protected  $joins  = [];
+    public  function innerJoin($table,$alias)
+    {
+        $this->joins[] = " INNER JOIN $table $alias ";
+        return $this;
+    }
+
+    public  function rightJoin($table,$alias)
+    {
+        $this->joins[] = " RIGHT JOIN $table $alias ";
+        return $this;
+
+    }
+
+    public  function leftJoin($table,$alias)
+    {
+        $this->joins[] = " RIGHT JOIN  $table $alias ";
+        return $this;
+    }
+
+    public  function fullJoin($table,$alias)
+    {
+        $this->joins[] = " FULL JOIN  $table $alias ";
+        return $this;
+    }
+
+
+    public  function  on($key,$value)
+    {
+        $condition = "$key=$value";
+        $this->joins[] = "ON $condition";
+        return $this;
+    }
+
+    public  function fetchJoins()
+    {
+        $joins = [];
+        if(count($this->joins)) {
+            foreach ($this->joins as $join) {
+                $joins[] = $join;
+            }
+            $this->sql .= implode(" ", $joins);
+        }
+    }
+}
