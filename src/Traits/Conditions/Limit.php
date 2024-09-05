fe06c4fd5c32c5b32d6185f8fee4a7e45b7a9a47
martin bamber <mbamber1986@gmail.com>
2024-09-05 14:09:14 +0100

    Added Limit Trait

---
+++ src/Traits/Conditions/Limit.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace LazarusPhp\Orm\Traits\Conditions;
+
+trait Limit
+{
+
+    public  $limit = [];
+    public function limit($start,$end=null)
+    {
+        $parammin = uniqid("limitMin_");
+        $paramend= uniqid("limitEnd_");
+
+        $this->limit[] =  " LIMIT :$parammin ";
+        $this->param[$parammin] = $start;
+        if(!is_null($end))
+            {
+                $this->limit[] .= ", :$paramend";
+                $this->param[$paramend] = $end;
+            }
+
+
+
+        return $this;
+    }
+
+
+    public function fetchLimit()
+    {
+        $mewLimit = [];
+        if(count($this->limit))
+        {
+            foreach ($this->limit as $limit)
+            {
+                $newLimit[] = $limit;
+            }
+            $this->sql .= implode(" ", $newLimit);
+        }
+    }
+}
