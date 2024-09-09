a16b9576be6209471ea6e52f9a89e588d1e63e4d
martin bamber <mbamber1986@gmail.com>
2024-09-09 09:44:38 +0100

    Updated Groups to support multiple groups

--- src/Traits/Conditions/Grouping.php
+++ src/Traits/Conditions/Grouping.php
@@ -4,19 +4,21 @@ namespace LazarusPhp\Orm\Traits\Conditions;
 trait Grouping
 {
     private $group = [];
-
-    public function groupBy($column)
+    public function groupBy(...$columns)
     {
     // Add the column directly to the group array.
-    $this->group[] = $column;
+        foreach ($columns as $column)
+        {
+            $this->group[] = $column;
+        }
     return $this;
     }
 
     public function fetchGroupBy()
     {
-        if (!empty($this->group) && count($this->group)===1) {
-        // Ensure the GROUP BY clause is added to the SQL query
-        $this->sql .= " GROUP BY " . implode(", ", $this->group);
+        if (!empty($this->group)) {
+            // Ensure the GROUP BY clause is added to the SQL query
+            $this->sql .= " GROUP BY " . implode(", ", $this->group);
         }
     }
 
