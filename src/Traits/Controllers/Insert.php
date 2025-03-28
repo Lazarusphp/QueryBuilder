7ed2375326687bdb8234a5c8eb97e6dad4ac8e78
martin bamber <mbamber1986@gmail.com>
2025-03-28 12:14:31 +0000

    Added Replace into Insert Trait

--- src/Traits/Controllers/Insert.php
+++ src/Traits/Controllers/Insert.php
@@ -3,13 +3,39 @@ namespace LazarusPhp\QueryBuilder\Traits\Controllers;
 
 trait Insert
 {
-    public function insert()
+    public function insert(array $data = [])
     {
-        $keys = implode(',',array_keys($this->param));
-        $placeholders = ':' . implode(', :', array_keys($this->param));
+        if (count($data) === 0) {
+            
+            $keys = implode(',', array_keys($this->param));
+            $placeholders = ':' . implode(', :', array_keys($this->param));
+        } else {
+            // If Array is passed
+            $keys = implode(',', array_keys($data));
+            $placeholders = ':' . implode(', :', array_keys($data));
+            $this->param = $data;
+        }
         $this->sql .= "INSERT INTO " . $this->table ;
         $this->sql .= " ($keys) ";
         $this->sql .= "VALUES($placeholders)";
-        return $this;
+        return $this->save($this->sql,$this->param);
+    }
+
+    public function replace(array $data = [])
+    {
+        if (count($data) === 0) {
+            
+            $keys = implode(',', array_keys($this->param));
+            $placeholders = ':' . implode(', :', array_keys($this->param));
+        } else {
+            // If Array is passed
+            $keys = implode(',', array_keys($data));
+            $placeholders = ':' . implode(', :', array_keys($data));
+            $this->param = $data;
+        }
+        $this->sql .= "REPLACE INTO " . $this->table ;
+        $this->sql .= " ($keys) ";
+        $this->sql .= "VALUES($placeholders)";
+        return $this->save($this->sql,$this->param);
     }
 }
